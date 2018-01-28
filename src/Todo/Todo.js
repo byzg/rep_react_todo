66466a940653be7aeb8d5ab982a03d9421b2d2bc
byzg <byzg00@gmail.com>
2018-01-28 14:21:03 +0300

    28.01.18: json-server and superagent

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import Request from 'superagent';
 
 import './Todo.css';
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: []
+    }
+  }
+
+  componentWillMount() {
+    Request
+      .get('http://localhost:3004/items')
+      .then((res)=> {
+        this.setState({todoList: res.body});
+      });
+  }
+
   render() {
-    const todoList = [
-      'To eat',
-      'To drink',
-      'To sleep'
-    ];
     return (
       <div className="Todo">
-        { todoList.map((item)=> <div>{ item }</div>) }
+        { this.state.todoList.map((item, i)=> <div key={i}>{ item.title }</div>) }
       </div>
     )
   }
